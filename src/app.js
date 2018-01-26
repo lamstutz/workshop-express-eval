1b5c12425125e60259e2042f672c1e1f69875349
lamstutz <louis.amstutz@gmail.com>
2018-01-26 15:22:20 +0100

    Exercice 4

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
 app.get('/', function (req, res) {
   res.send('Hello from serveur workshop-express-eval');
 });
 
 app.get('/bingo', function (req, res) {
-  res.send('Bingo');
+  fs.readFile('src/numbers.txt', 'utf8', (err, data) => {
+    if (err) throw err;
+    res.send(data.split('\n').slice(0, -1).join(', '));
+  });
 });
 
 module.exports = app;
